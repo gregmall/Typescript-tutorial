ba9e698a5bceca1235bf3a493535eedcf517ba78
gregmall <gregmall157@gmail.com>
2022-02-10 15:29:54 -0800

    lesson 6

--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,52 @@
-// explicit types
+// // explicit types
 
-let character: string;
-let age: number;
-let logged: boolean;
+// let character: string;
+// let age: number;
+// let logged: boolean;
 
-age = 33;
-logged= false;
-character = 'moth'
+// age = 33;
+// logged= false;
+// character = 'moth'
 
-//array
+// //array
 
-let ninjas: string[] = [];
+// let ninjas: string[] = [];
 
-// union types
+// // union types
 
-let mixed: (string|number|boolean)[] = [];
-mixed.push(33)
-mixed.push(false)
-console.log(mixed)
+// let mixed: (string|number|boolean)[] = [];
+// mixed.push(33)
+// mixed.push(false)
+// console.log(mixed)
 
 
-let uid: string|number;
-uid = 12;
-uid = 'twelve'
+// let uid: string|number;
+// uid = 12;
+// uid = 'twelve'
 
-//object;
-let ninjaOne: object;
-ninjaOne = { name: 'yoshi', age: 30 };
+// //object;
+// let ninjaOne: object;
+// ninjaOne = { name: 'yoshi', age: 30 };
 
-let ninjaTwo: {
-    name: string,
-    age: number,
-    belt: string
+// let ninjaTwo: {
+//     name: string,
+//     age: number,
+//     belt: string
+// }
+
+// console.log(ninjaOne)
+
+let age: any = 23;
+age = true;
+
+age = {
+    name: 'luigi'
 }
 
-console.log(ninjaOne)
+
+let mixed: any[] =[];
+mixed.push(age)
+console.log(mixed)
+let ninja: { name: any, age: any}
+ninja = { name: 'steve', age: 33}
+console.log(ninja)
