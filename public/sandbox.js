3edf4d3c3d676e9b7fb4fc41f760da52a1c6ba4c
gregmall <gregmall157@gmail.com>
2022-02-15 11:05:13 -0800

    lesson11

--- public/sandbox.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// // explicit types
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
-};
-const greetAgain = (user) => {
-    console.log(`${user.name} says hello`);
-};
